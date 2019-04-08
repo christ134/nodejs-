f996e5c4fc81c962c3243a5c187e661b2a4a04bc
ckrollin <Chris.MAT360@protonmail.com>
2019-04-08 15:56:41 +0530

    luwyet

--- app.js
+++ app.js
@@ -12,7 +12,9 @@ app.get('/home',(req,res)=>{
 res.send('welcome to my homepage');
 });
 app.get('/contacts',(req,res)=>{
-    res.send('my contacts')
+    res.send('my contacts hello')
 })
 
+
+app.use(express.static('nodero'))
 app.listen(3000);
